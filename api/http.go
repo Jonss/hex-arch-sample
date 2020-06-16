f4d0300a9af2a6f84345e6006920a1a93378d53c
Joao Santana <joaosantana.ti@gmail.com>
2020-06-16 19:59:15 -0300

    finishing hex-arch-sample

---
+++ api/http.go
@@ -0,0 +1,87 @@
+package api
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	js "github.com/Jonss/hex-arch-sample/serializer/json"
+	ms "github.com/Jonss/hex-arch-sample/serializer/msgpack"
+	"github.com/Jonss/hex-arch-sample/shortener"
+	"github.com/go-chi/chi"
+	"github.com/pkg/errors"
+)
+
+// RedirectHandler is
+type RedirectHandler interface {
+	Get(http.ResponseWriter, *http.Request)
+	Post(http.ResponseWriter, *http.Request)
+}
+
+type handler struct {
+	redirectService shortener.RedirectService
+}
+
+// NewHandler instantiate a handler
+func NewHandler(redirectService shortener.RedirectService) RedirectHandler {
+	return &handler{redirectService: redirectService}
+}
+
+func (h *handler) serializer(contentType string) shortener.RedirectSerializer {
+	if contentType == "application/x-msgpack" {
+		return &ms.Redirect{}
+	}
+	return &js.Redirect{}
+}
+
+func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
+	code := chi.URLParam(r, "code")
+	redirect, err := h.redirectService.Find(code)
+	if err != nil {
+		if errors.Cause(err) == shortener.ErrRedirectInvalid {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, redirect.URL, http.StatusMovedPermanently)
+}
+
+func (h *handler) Post(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("Content-Type")
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	redirect, err := h.serializer(contentType).Decode(requestBody)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	err = h.redirectService.Store(redirect)
+	if err != nil {
+		if errors.Cause(err) == shortener.ErrRedirectInvalid {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	responseBody, err := h.serializer(contentType).Encode(redirect)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	setupResponse(w, contentType, responseBody, http.StatusCreated)
+}
+
+func setupResponse(w http.ResponseWriter, contentType string, body []byte, statusCode int) {
+	w.Header().Set("Content-type", contentType)
+	w.WriteHeader(statusCode)
+	_, err := w.Write(body)
+	if err != nil {
+		log.Println(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,16 @@ module github.com/Jonss/hex-arch-sample
 go 1.14
 
 require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-redis/redis v6.15.8+incompatible
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf // indirect
-	gopkg.in/dealancer/validate.v2 v2.1.0 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	go.mongodb.org/mongo-driver v1.3.4
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+	gopkg.in/dealancer/validate.v2 v2.1.0
 )
